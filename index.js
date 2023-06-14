89c15cc17d4dcf2da5d68380c6d814b3a722dafb
Ahmed Omar Salim Adnan <44505932+RionelxSlovesky@users.noreply.github.com>
2023-06-14 13:48:24 +0600

    Implemented Add Class
    Now can add class to the database

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middlewares
 app.use(express.json());
@@ -13,80 +12,99 @@ app.use(cors());
 //mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.spr5boq.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
 
 app.get("/", (req, res) => {
   res.send("Summer Camp Running");
 });
 
-
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-     //   await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("admin").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    //   await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
 
-      const usersCollection = client.db("summerCampDB").collection("users");
+    const usersCollection = client.db("summerCampDB").collection("users");
+    const classesCollection = client.db("summerCampDB").collection("classes");
 
-      // USERS
-      //get user by id
-      app.get('/users/:id', async(req,res) => {
-        const id = req.params.id;
-        const query = { _id: new ObjectId(id)}
-        const result = await usersCollection.findOne(query);
-        res.send(result)
-      })
+    // USERS
+    //get user by id
+    app.get("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
 
-      //get user by email
-      app.get('/users', async(req,res) => {
+    //get user by email
+    app.get("/users", async (req, res) => {
+      try {
         const email = req.query.email;
-        const query = { email: email }
+        const query = { email: email };
         const result = await usersCollection.findOne(query);
-        res.send(result)
-      })
-
-      //posting user info to database
-      app.post('/users', async(req, res) => {
-        const user = req.body;
-        const query = {email: user.email}
-        const existingUser = await usersCollection.findOne(query)
-        if(existingUser) {
-          return res.send({message: 'user already exists'})
+        if(result){
+          res.send(result);
+        }else {
+          res.send({})
         }
-        const result = await usersCollection.insertOne(user);
-        res.send(result)
-      })
+        
+      } finally {
+
+      }
+      
+    });
+
+    //posting user info to database
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await usersCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "user already exists" });
+      }
+      const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    // user by role
+    app.get("/usersRole/:role", async (req, res) => {
+      const role = req.params.role;
+      const query = { role: role };
+      const result = await usersCollection.find(query).toArray();
+      res.send(result);
+    });
+
+
+    // CLASSES
+
+    // posting class
+    app.post("/class", async (req,res) => {
+      const item = req.body;
+      console.log(item)
+      const result = await classesCollection.insertOne(item)
+      res.send(result)
+    })
 
 
 
 
-      // user by role
-      app.get('/usersRole/:role', async(req,res) => {
-        const role = req.params.role;
-        const query = {role: role}
-        const result = await usersCollection.find(query).toArray()
-        res.send(result)
-      })
 
-
-    } finally {
-      // Ensures that the client will close when you finish/error
+  } finally {
+    // Ensures that the client will close when you finish/error
     //   await client.close();
-    }
   }
-  run().catch(console.dir);
-
-
-
-
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log("Listening to PORT: " + port);
+  console.log("Listening to PORT: " + port);
 });
